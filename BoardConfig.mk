3007f4c4af62e594fc590f54e760239d0d2ddfbf
AnanJaser <ananjaser@gmail.com>
2020-06-22 19:18:33 +0400

    board: use custom bootimgmk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,7 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/herolte/mkbootimg.mk
 TARGET_PREBUILT_KERNEL := device/samsung/herolte/kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
